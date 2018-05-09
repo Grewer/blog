661cb99e3ce23b4552cd9a3b2eb77d0778a241fb
zhangyu <grewer@grewer.cn>
2018-05-09 22:29:04 +0800

    feat(module) : mock 模拟说句

--- src/module/home/index.js
+++ src/module/home/index.js
@@ -1,20 +1,31 @@
 import React, {Component} from 'react';
-import Article from '../../components/article'
+// import Article from '../../components/article'
 import './index.less'
-// ajax 获取数据
-const array = [{title: '标题1', time: '2018-5-8', content: 'qwerty'}]
 // 后面使用 saga 异步获取
+
+let Mock = require('mockjs')
+let data = Mock.mock({
+  'list|10': [{
+    'id|+1': 1,
+    'title': '@sentence(3, 5)',
+    'content': '@paragraph',
+    'time': '@date("yyyy-MM-dd")',
+    'clicks|1-100': 1
+  }]
+})
+
+
 
 class Home extends Component {
   render() {
     return (<div className="body">
       {
-        array.map(i => {
+        data.list.map((i, index) => {
           return (
-            <div className="articleBox">
+            <div className="articleBox" key={index}>
               <p>{i.title}</p>
               <div>{i.content}</div>
-              <p>{i.time}</p>
+              <span>{i.time}</span><span>{i.clicks}</span>
             </div>
           )
         })
