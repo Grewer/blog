6f39a2db9f23177f17d4506f0a38d90ffc894c30
zhangyu <grewer@grewer.cn>
2018-05-11 23:38:23 +0800

    feat(module):文章块的样式

--- src/module/home/index.js
+++ src/module/home/index.js
@@ -4,30 +4,39 @@ import './index.less'
 // 后面使用 saga 异步获取
 import {connect} from 'react-redux';
 
-
+function filterContent(val) {
+  return val.length > 300 ? val.substr(0, 300) + '...' : val
+}
 
 class Home extends Component {
   render() {
     const {ArticleList} = this.props
     return (
       <div className="body">
-      {
-        ArticleList.map((i, index) => {
-          return (
-            <div className="articleBox" key={index}>
-              <p>{i.title}</p>
-              <div>{i.content}</div>
-              <span>{i.time}</span><span>{i.clicks}</span>
-            </div>
-          )
-        })
-      }
-    </div>);
+        {
+          ArticleList.map((i, index) => {
+            return (
+              <div className="articleBox" key={index}>
+                <p>{i.title}</p>
+                <div>{filterContent(i.content)}</div>
+                <span><svg class="icon" aria-hidden="true">
+                  <use xlinkHref="#icon-shijian"></use>
+                  </svg>
+                  {i.time}</span><span><svg class="icon ft-20" aria-hidden="true">
+                  <use xlinkHref="#icon-tubiaozhizuomoban"></use>
+                  </svg>{i.clicks}</span>
+              </div>
+            )
+          })
+        }
+      </div>);
   }
 }
+
 function showData(state) {
   return {
     ArticleList: state
   }
 }
+
 export default connect(showData)(Home);
