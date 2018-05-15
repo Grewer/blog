c4756767728a507556f81757f2fde9785abb9c36
zhangyu <grewer@grewer.cn>
2018-05-15 23:29:50 +0800

    feat(module) : 响应式列表的样式,动画

--- src/module/header/phoneList.js
+++ src/module/header/phoneList.js
@@ -1,16 +1,43 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import QueueAnim from 'rc-queue-anim';
+
+const menu = [
+  {
+    path: '/',
+    name: '首页',
+    icon: 'shouye'
+  },
+  {
+    path: '/category',
+    name: '分类',
+    icon: 'fenlei1'
+  },
+  {
+    path: '/list',
+    name: '归档',
+    icon: 'guidangxiangmu'
+  }
+]
 
 class PhoneList extends Component {
   render() {
     let {phoneListStatus} = this.props
-    return (<div className={(phoneListStatus ? 'block' : 'hidden') +' '+ 'phoneList'}>
-      <ul>
-        <li key="1">1</li>
-        <li key="2">2</li>
-        <li key="3">3</li>
-      </ul>
-    </div>);
+    return (<QueueAnim type={['right', 'left']}
+                       ease={['easeOutQuart', 'easeInOutQuart']} className="phoneList">
+      {
+        phoneListStatus ? <ul key="ul">{
+            menu.map(i => {
+              return (<li key={i.path}>
+                <svg className="icon" aria-hidden="true">
+                  <use xlinkHref={'#icon-' + i.icon}></use>
+                </svg>
+                {i.name}</li>)
+            })}
+          </ul>
+          : null
+      }
+    </QueueAnim>);
   }
 }
 
